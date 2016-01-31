663d69a633d767ed471fcd973cb1f21f618531bd
Andrea <roundcrisis@gmail.com>
2016-01-31 09:37:14 +0000

    first version

---
+++ src/fsharp/HelloMonad/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+
+// Imperative version
+let hello() = printfn "Hello World!"
+
+
+// 
+let Write s = printfn s
+
+type IOProgram = Write of string
+
+let pureHello () =
+  Write("Hello World")
+
+let run (program:IOProgram) =
+    match program with
+    | Write msg -> printfn "%A" msg
+
+let runPureHello() =  pureHello() |> run
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
